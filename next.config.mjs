00620268ef2adf5af2e5c31900a871b22ab4b976
Sadara <mkaawaja5000@gmail.com>
2026-07-01 21:28:30 +0300

    fix the next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  serverExternalPackages: ['jsdom', 'isomorphic-dompurify'],
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'images.unsplash.com' },
